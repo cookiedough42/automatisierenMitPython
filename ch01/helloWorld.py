1c13227db08c3c2f7467c74c488074bab29499ff
matze <thundermatze@gmail.com>
2020-02-15 20:27:10 +0100

    add helloWorld.pi

---
+++ ch01/helloWorld.py
@@ -0,0 +1,9 @@
+# Dieses Programm sagt "Hallo" und fragt nach Ihrem Namen.
+print('Hello World')
+print('What is your name?')
+myName = input()
+print('It is good to see you, ' + myName)
+print('The length of your name is ' + str(len(myName)) + '.')
+print('What is your age, ' + myName +'?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
